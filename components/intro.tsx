d7a29237293caa5624d41a0afcf18a94d6d1dfad
ks3669 <157922493+ks3669@users.noreply.github.com>
2025-03-03 01:09:25 -0700

    Update intro.tsx
    increased DP size

--- components/intro.tsx
+++ components/intro.tsx
@@ -35,8 +35,8 @@ export default function Intro() {
             <Image
               src={githubdp}
               alt="Karthik portrait"
-              width="220"
-              height="220"
+              width="320"
+              height="320"
               quality="95"
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
